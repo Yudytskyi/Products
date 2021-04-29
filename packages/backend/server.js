88a64aa99896055df390c977a0d4ce713d268dbc
Yudytskyi <uan0503631989@gmail.com>
2021-04-29 22:08:01 +0300

    refactor server

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,10 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
-const server = http.createServer(app);
-
 const port = process.env.port ?? 5000;
 
-server.listen(port, () => {
+http.createServer(app).listen(port, () => {
   console.log(`listen port:${port}`);
 });
