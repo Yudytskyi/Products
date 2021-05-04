0c45f7aa8ed50ff397e2569d16e14e9ff4ae5dbd
Yudytskyi <uan0503631989@gmail.com>
2021-05-04 21:58:16 +0300

    add associate ProductType to Attribute

--- packages/backend/models/producttype.js
+++ packages/backend/models/producttype.js
@@ -8,7 +8,8 @@ module.exports = (sequelize, DataTypes) => {
           model: Attribute,
           unique: true,
         },
-      });
+      }),
+        ProductType.hasMany(Attribute);
     }
   }
   ProductType.init(
