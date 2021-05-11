f00c806d8319ee3f4898b8922032ab9c66ae8f94
Yudytskyi <uan0503631989@gmail.com>
2021-05-11 18:06:04 +0300

    remove trash files

--- packages/backend/services/index.js
+++ packages/backend/services/index.js
@@ -1,10 +1,6 @@
 module.exports.prepareObjects = require('./prepareObjects');
 
-module.exports.prepareProductTypes = require('./prepareProductTypes');
-
 module.exports.getValueByKeys = require('./getValueByKeys');
-
-module.exports.getAllFieldsOfObject = require('./getAllFieldsOfObject');
 
 module.exports.mergeObjects = require('./mergeObjects');
 
