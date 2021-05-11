16716acae14552de8efeccf6285eb9397faa00eb
Yudytskyi <uan0503631989@gmail.com>
2021-05-11 16:41:22 +0300

    create deleteByIdProductType

--- packages/backend/controllers/productTypeControllers/deleteByIdProductType.js
+++ packages/backend/controllers/productTypeControllers/deleteByIdProductType.js
@@ -1,12 +1,19 @@
 const { ProductType } = require('../../models');
-const createError = require('http-errors');
 
 const deleteByIdProductType = async (req, res, next) => {
   const {
     params: { productTypeId },
   } = req;
+
   try {
-    res.status(200).send(`deleted productType whit id:${productTypeId}`);
+    const productTypeInstance = await ProductType.findByPk(productTypeId);
+
+    productTypeInstance
+      ? (await productTypeInstance.destroy()) &&
+        res.status(200).send(`ProductType by id:${productTypeId} deleted`)
+      : res
+          .status(404)
+          .send(`ProductType by id: ${productTypeId} does not exist`);
   } catch (err) {
     return next(err);
   }
