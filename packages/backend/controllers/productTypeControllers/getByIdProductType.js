fab7b17cb4435501fe6c96117e5e973e405e2fcb
Yudytskyi <uan0503631989@gmail.com>
2021-05-11 16:14:58 +0300

    create getByIdProductType

--- packages/backend/controllers/productTypeControllers/getByIdProductType.js
+++ packages/backend/controllers/productTypeControllers/getByIdProductType.js
@@ -1,12 +1,23 @@
 const { ProductType } = require('../../models');
-const createError = require('http-errors');
+const { prepareObjects } = require('../../services');
+const {
+  db: { modelPreparedProductType },
+} = require('../../config/db.json');
 
 const getByIdProductType = async (req, res, next) => {
   const {
     params: { productTypeId },
   } = req;
   try {
-    res.status(200).send(`productType whit id:${productTypeId}`);
+    const foundProductType = await ProductType.findByPk(productTypeId);
+
+    foundProductType
+      ? res.status(200).send({
+          data: prepareObjects(foundProductType, modelPreparedProductType),
+        })
+      : res
+          .status(404)
+          .send(`ProductType by id: ${productTypeId} does not exist`);
   } catch (err) {
     return next(err);
   }
--- packages/backend/middlewares/validateProduct.js
+++ packages/backend/middlewares/validateProduct.js
@@ -1,27 +1,23 @@
 const { productCreateSchema, productUpdateSchema } = require('./schemas');
 
-const validateProduct = {
-  async onCreate(req, res, next) {
-    const { body } = req;
-    try {
-      await productCreateSchema.validate(body);
+module.exports.onCreate = async (req, res, next) => {
+  const { body } = req;
+  try {
+    await productCreateSchema.validate(body);
 
-      next();
-    } catch (err) {
-      res.status(400).send({ message: err.message ?? massage });
-    }
-  },
-
-  async onUpdate(req, res, next) {
-    const { body } = req;
-    try {
-      await productUpdateSchema.validate(body);
-
-      next();
-    } catch (err) {
-      res.status(400).send({ message: err.message ?? massage });
-    }
-  },
+    next();
+  } catch (err) {
+    res.status(400).send({ message: err.message ?? massage });
+  }
 };
 
-module.exports = validateProduct;
+module.exports.onUpdate = async (req, res, next) => {
+  const { body } = req;
+  try {
+    await productUpdateSchema.validate(body);
+
+    next();
+  } catch (err) {
+    res.status(400).send({ message: err.message ?? massage });
+  }
+};
