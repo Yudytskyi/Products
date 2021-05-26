cb1aaafef902605941b3b3e42058e3daa60d03f2
Yudytskyi <uan0503631989@gmail.com>
2021-05-26 20:20:07 +0300

    refactor signUpSchema

--- packages/backend/middlewares/schemas/signUpSchema.js
+++ packages/backend/middlewares/schemas/signUpSchema.js
@@ -8,16 +8,25 @@ const {
 } = require('../../config/db.json');
 const { passwordRule } = require('../../config/jwt');
 
-const signUpSchema = yup.object().shape({
-  firstName: yup.string().trim().required(),
-  lastName: yup.string().trim().required(),
-  displayName: yup.string().trim().required(),
-  email: yup.string().trim().email().required(),
-  password: yup
-    .string()
-    .matches(...passwordRule)
-    .required(),
-  role: yup.string().oneOf(roles).required(),
-});
+const signUpSchema = yup
+  .object()
+  .shape({
+    data: yup.array().of(
+      yup.object().shape({
+        user: yup.object().shape({
+          firstName: yup.string().trim().required(),
+          lastName: yup.string().trim().required(),
+          userName: yup.string().trim().required(),
+          email: yup.string().trim().email().required(),
+          password: yup
+            .string()
+            .matches(...passwordRule)
+            .required(),
+          role: yup.string().oneOf(roles).required(),
+        }),
+      })
+    ),
+  })
+  .noUnknown({ unknownKey: true });
 
 module.exports = signUpSchema;
