e9310b755cfba6cc216029cb41abe8dc2c8cb812
Yudytskyi <uan0503631989@gmail.com>
2021-06-11 10:13:16 +0300

    refactor testFieldInSchema

--- packages/backend/services/testFieldInSchema.js
+++ packages/backend/services/testFieldInSchema.js
@@ -2,7 +2,8 @@
 
 const testFieldInSchema = (value, context, fieldName) => {
   const typeName = context.from[1].value.productType.typeName;
-  return (value !== undefined && typeName === fieldName) ||
+  return (typeName === fieldName && value !== undefined) ||
+    (typeName === undefined && value !== undefined) ||
     (value === undefined && typeName !== fieldName)
     ? true
     : false;
