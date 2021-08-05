43495feda827e260cb8ad427e23993762cc1b037
Yudytskyi <uan0503631989@gmail.com>
2021-08-05 14:18:36 +0300

    refoctor Spinner Page

--- packages/frontend/src/components/Spinner/index.js
+++ packages/frontend/src/components/Spinner/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import ClipLoader from 'react-spinners/ClipLoader';
-import { css } from '@emotion/react';
-import styles from './Spinner.module.sass';
-
-const size =
-  window.innerWidth < window.innerHeight
-    ? window.innerWidth / 2
-    : window.innerHeight / 2;
-
-const override = css`
-  border-color: #46568a;
-  border: 30px solid #46568a;
-  animation: animation-s8tf20 1s 0s infinite cubic-bezier(1, 0, 1, 0.6);
-`;
+import styles from './styles.module.scss';
 
 const Spinner = () => {
   return (
-    <div className={styles.loaderContainer}>
-      <ClipLoader css={override} size={size} />
+    <div className={styles.container}>
+      <div className={styles.rotating} />
     </div>
   );
 };
--- packages/frontend/src/components/index.js
+++ packages/frontend/src/components/index.js
@@ -1,6 +1,6 @@
 import Spinner from './Spinner';
 import NotFound from './NotFound';
-import { LoginSignupForm } from './forms/LoginSignupForm';
+import { AuthForms } from './forms/AuthForms';
 import { ProductForms } from './forms/ProductForms';
 
-export { Spinner, NotFound, LoginSignupForm, ProductForms };
+export { Spinner, NotFound, AuthForms, ProductForms };
